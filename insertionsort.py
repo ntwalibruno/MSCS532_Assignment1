b976ee990823f61142434d094be59bd55a6e2436
ntwalibruno <nbahongere30541@ucumberlands.edu>
2024-10-26 19:40:17 -0500

    Insertion sort in in monotonically decreasing order.

--- insertionsort.py
+++ insertionsort.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+def sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
+        arr[j + 1] = key
+
+array = [50,560,100,40,20]
+
+print("Original array: ", array)
+
+sort(array)
+
+print("Sorted array  : ", array)
